24ded8bddfc31472e4f8b83e31faa3bc83b933c5
itsaFan <steffansim@gmail.com>
2023-02-26 00:23:32 +0800

    prog

---
+++ webproject/next-demo-project/pages/events/[eventId].tsx
@@ -0,0 +1,7 @@
+export default function EventDetailPage () {
+    return (
+        <div>
+            <h1>This is single event detail page</h1>
+        </div>
+    )
+}
---
+++ webproject/next-demo-project/pages/events/index.tsx
@@ -0,0 +1,7 @@
+export default function EventsPage() {
+    return (
+        <div>
+            <h1>Display All Events page</h1>
+        </div>
+    )
+}
