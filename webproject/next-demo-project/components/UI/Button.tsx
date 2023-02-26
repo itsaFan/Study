b10c8284162227d49eff57b858af4f2fd9a1dee7
itsaFan <steffansim@gmail.com>
2023-02-26 18:46:31 +0800

    prog

--- webproject/next-demo-project/components/UI/Button.tsx
+++ webproject/next-demo-project/components/UI/Button.tsx
@@ -5,12 +5,22 @@ import classes from "./css/Btn.module.css";
 type Props = {
   link: string;
   children: ReactNode;
+  onClick?: () => void;
 };
 
 export default function Button(props: Props) {
+  if (props.link) {
+    return (
+      <div>
+        <Link href={props.link} className={classes.btn}>
+          {props.children}
+        </Link>
+      </div>
+    );
+  }
   return (
-    <div>
-      <Link href={props.link} className={classes.btn}>{props.children}</Link>
-    </div>
+    <button className={classes.btn} onClick={props.onClick}>
+      {props.children}
+    </button>
   );
 }
--- webproject/next-demo-project/pages/events/[eventId].tsx
+++ webproject/next-demo-project/pages/events/[eventId].tsx
@@ -1,6 +1,9 @@
 import EventContent from "@/components/event-detail/event-content";
 import EventLogistics from "@/components/event-detail/event-logistics";
 import EventSummary from "@/components/event-detail/event-summary";
+import Button from "@/components/UI/Button";
+import ErrorAlertUI from "@/components/UI/error-alert-ui";
+import PageHead from "@/components/UI/page-title";
 import { getEventById } from "@/dummy-data";
 import { useRouter } from "next/router";
 import { Fragment } from "react";
@@ -12,11 +15,21 @@ export default function EventDetailPage() {
   const event = getEventById(eventId);
 
   if (!event) {
-    return <p>No event found~</p>;
+    return (
+      <Fragment>
+        <ErrorAlertUI>
+          <p>Sorry, No Event Found</p>
+        </ErrorAlertUI>
+        <div className="center">
+          <Button link="/events">Back to All Events</Button>
+        </div>
+      </Fragment>
+    );
   }
 
   return (
     <Fragment>
+      <PageHead pageTitle="Event Detail" />
       <EventSummary title={event.title} />
       <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title} />
       <EventContent>
